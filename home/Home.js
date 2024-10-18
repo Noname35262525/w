48b0343dc28df057c033b38e58de9b0ca331e851
Noname35262525 <141251732+Noname35262525@users.noreply.github.com>
2024-10-18 18:42:54 +0100

    balk

--- home/Home.js
+++ home/Home.js
@@ -3,18 +3,18 @@ let context = canvas.getContext("2d");
 
 function drawLines(){
     context.beginPath();
-    context.moveTo(0,0);
+    context.moveTo(0,200);
     context.lineTo(300,300);
-    context.moveTo(300,0);
+    context.moveTo(300,200);
     context.lineTo(0,300);
     context.stroke();
 };
 function drawText(){
-    context.font = "50px sans";
+    context.font = "40px sans";
     console.fillStyle = "Blue";
     context.textAlign = "center";
-    context.fillText("PICTURES?",canvas.width / 2,canvas.height / 2);
+    context.fillText("box to fill space",canvas.width / 2,canvas.height / 2);
+    context.fillText("BLANK",canvas.width / 2,canvas.height / 4);
 };
-
-canvas.addEventListener("click", drawLines);
-canvas.addEventListener("mouseenter", drawText);
+drawText();
+drawLines();
